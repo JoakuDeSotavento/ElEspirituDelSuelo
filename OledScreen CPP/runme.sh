8b26e07ab30251101fdb36b100c723e2d8053be7
Joaku De Sotavento <joaquindiazescultor@gmail.com>
2025-08-13 17:17:15 +0200

    Update runme.sh

--- OledScreen CPP/runme.sh
+++ OledScreen CPP/runme.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-PROJECT_DIR=$(dirname "$0") #note: this is _not_ the variable of the same name used `make` below: that's the Makefile's own
-cd $PROJECT_DIR
+#se comenta todo eso para que no compile el proyecto al inicio, porque tiraba el proyecto del boot
 
-[ -z "$BELA_HOME" ] && BELA_HOME=$(realpath "`pwd`/../../")
-[ -z "$PROJECT_NAME" ] && PROJECT_NAME=$(basename "`pwd`")
+#PROJECT_DIR=$(dirname "$0") #note: this is _not_ the variable of the same name used `make` below: that's the Makefile's own
+#cd $PROJECT_DIR
 
-set -e
-make -C $BELA_HOME CFLAGS=-I\$\(PROJECT_DIR\)/u8g2/csrc CPPFLAGS=-I\$\(PROJECT_DIR\)/u8g2/csrc PROJECT="$PROJECT_NAME" $([ 0 -eq "$#" ] && echo run || echo $@)
+#[ -z "$BELA_HOME" ] && BELA_HOME=$(realpath "`pwd`/../../")
+#[ -z "$PROJECT_NAME" ] && PROJECT_NAME=$(basename "`pwd`")
+
+#set -e
+#make -C $BELA_HOME CFLAGS=-I\$\(PROJECT_DIR\)/u8g2/csrc CPPFLAGS=-I\$\(PROJECT_DIR\)/u8g2/csrc PROJECT="$PROJECT_NAME" $([ 0 -eq "$#" ] && echo run || echo $@)
+
+cd /root/Bela/projects/pantallita
+chmod +x pantallita
+./pantallita
